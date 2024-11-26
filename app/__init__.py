1d83d708a7d5174f182ecb8294d951030de63de2
David16010 <matheus.felix2153@gmail.com>
2024-11-26 14:32:26 -0300

    my new commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
-#comentario
+from flask import FLask
+from config import Config
+
+def create_app():
+    app = FLask(__name__)
+
+    from .import routes
+    routes.init_app(app)
+    return app
+
+
+  
---
+++ app/run.py
@@ -0,0 +1,7 @@
+From app import create_app
+
+app = create_app()
+
+if__name__ == __main__:
+    app.run(debug = True)
+    
